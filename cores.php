c2ef6c3c87d637f04f50dba1ff399f3e78212f62
joaohenrique35 <83438320+joaohenrique35@users.noreply.github.com>
2023-04-24 23:18:26 -0300

    Add files via upload

---
+++ cores.php
@@ -0,0 +1,43 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Formatação</title>
+		
+		
+		<?php
+			$texto = $_GET["texto"];
+			$tamanho = $_GET["tamanho"];
+			$cor = $_GET["cor"];
+			$corfundo = $_GET["corfundo"];
+		?>
+		
+		<style>
+			body {
+				background-color: <?php echo $corfundo; ?>;}
+			span.texto {
+				font-size: <?php echo $tamanho;?>;
+				color: <?php echo $cor;?>;
+				
+			}
+		</style>
+
+
+</head>
+
+
+<body>
+	<h1> Formatação CSS e PHP</h1>
+
+			<?php
+			echo "<span class = 'texto'>$texto <span>";
+			?>
+
+			<p><a href="cores.html"> Voltar </a></p>
+
+
+	
+</body>
+</html>
